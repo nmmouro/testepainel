fcdfbba41f54b5b95479fa53c51d6dd56c8de819
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-15 22:30:00 +0000

    Atualização do Painel Frota

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -1,20 +1,160 @@
 import { get }
 from "../services/api.js";
 
-async function carregarOcorrencias() {
+const cards =
+  document.querySelector("#cards");
+
+async function carregar() {
 
   const resultado =
     await get(
       "listarLancamentos"
     );
 
-  if (!resultado.sucesso)
+  if (!resultado.sucesso) {
+
+    cards.innerHTML = `
+      <p>
+        ${resultado.erro}
+      </p>
+    `;
+
     return;
 
-  renderizarCards(
+  }
+
+  renderizar(
     resultado.dados
   );
 
 }
 
-carregarOcorrencias();
+function renderizar(
+  registros
+) {
+
+  cards.innerHTML = "";
+
+  if (
+    registros.length === 0
+  ) {
+
+    cards.innerHTML =
+      "<p>Nenhuma ocorrência em andamento.</p>";
+
+    return;
+
+  }
+
+  registros.forEach(
+    registro => {
+
+      cards.innerHTML += `
+
+      <div class="card">
+
+        <h3>
+          ${registro.veiculo}
+        </h3>
+
+        <p>
+
+          <strong>
+            Motorista:
+          </strong>
+
+          ${registro.nome}
+
+        </p>
+
+        <p>
+
+          <strong>
+            Motivo:
+          </strong>
+
+          ${registro.motivo}
+
+        </p>
+
+        <p>
+
+          <strong>
+            KM Inicial:
+          </strong>
+
+          ${registro.kmInicial}
+
+        </p>
+
+        <p>
+
+          <strong>
+            Status:
+          </strong>
+
+          ${registro.status}
+
+        </p>
+
+        <div class="acoes">
+
+          <button
+            class="btn-editar"
+            onclick="
+              editar('${registro.id}')
+            ">
+            Editar
+          </button>
+
+          <button
+            class="btn-excluir"
+            onclick="
+              excluirRegistro('${registro.id}')
+            ">
+            Excluir
+          </button>
+
+        </div>
+
+      </div>
+
+      `;
+
+    });
+
+}
+
+window.editar = function(id) {
+
+  location.href =
+    `cadastro.html?id=${id}`;
+
+};
+
+window.excluirRegistro =
+async function(id) {
+
+  const confirmar =
+    confirm(
+      "Excluir registro?"
+    );
+
+  if (!confirmar)
+    return;
+
+  const resultado =
+    await get(
+      "excluirLancamento",
+      { id }
+    );
+
+  alert(
+    resultado.mensagem
+  );
+
+  carregar();
+
+};
+
+carregar();
